a36a5ef83d196d46238c250b94b66761e27e27e9
Dana-Kaziyeva <dana.kaazieva@gmail.com>
2023-10-13 15:42:14 +0600

    Changing the place of elements

--- app/src/main/java/com/example/happy_birthday_app/MainActivity.kt
+++ app/src/main/java/com/example/happy_birthday_app/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.happy_birthday_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happy_birthday_app.ui.theme.Happy_Birthday_appTheme
 
@@ -26,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //                    Greeting("Android")
+                    GreetingText(message = "Happy Birthday Teacher!", from = "from Dana")
                 }
             }
         }
@@ -41,17 +47,21 @@ class MainActivity : ComponentActivity() {
 //}
 @Composable
 fun GreetingText(message : String, from: String = "from Dana", modifier: Modifier = Modifier){
-    Column (modifier = modifier){
+    Column (modifier = modifier.padding(8.dp), verticalArrangement = Arrangement.Center){
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 97.sp,
             lineHeight = 100.sp,
-            color = Color.Green
+            color = Color.Red,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
             fontSize = 36.sp,
-            color = Color.Red
+            color = Color.White,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
@@ -61,6 +71,6 @@ fun GreetingText(message : String, from: String = "from Dana", modifier: Modifie
 fun GreetingPreview() {
     Happy_Birthday_appTheme {
         GreetingText(message = "Happy Birthday Teacher!")
-        
+
     }
 }
